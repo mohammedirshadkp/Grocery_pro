dfc73c03237a34d3afa48972c43b8133f4deafba
Mohammed Irshad <mohammedirshadkp500@gmail.com>
2024-03-31 17:35:17 +0530

    first commit grocery

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'eGrocery',
+      title: 'eGrocery ',
       theme: AppTheme.defaultTheme,
       onGenerateRoute: RouteGenerator.onGenerate,
       initialRoute: AppRoutes.onboarding,
