7f5facb977dc6c5f98c7db06705bc0e7c039498b
Mohammed Irshad <mohammedirshadkp500@gmail.com>
2024-04-06 17:49:17 +0530

    bug resolved

--- lib/core/components/app_back_button.dart
+++ lib/core/components/app_back_button.dart
@@ -4,7 +4,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import '../constants/constants.dart';
 
 class AppBackButton extends StatelessWidget {
-  /// Custom Back labelLarge with a custom ICON for this
+  /// Custom Back labelLarge with a custom ICON for thi
   const AppBackButton({
     Key? key,
   }) : super(key: key);
