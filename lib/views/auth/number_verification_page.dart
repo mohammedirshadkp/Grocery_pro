0eac3fc382b3c913fec68d24668afafad61fa4ab
Mohammed Irshad <mohammedirshadkp500@gmail.com>
2024-05-01 20:32:01 +0530

    login or signUp page

--- lib/views/auth/number_verification_page.dart
+++ lib/views/auth/number_verification_page.dart
@@ -60,7 +60,7 @@ class VerifyButton extends StatelessWidget {
       child: ElevatedButton(
         onPressed: () {
           showGeneralDialog(
-            barrierLabel: 'Dialog',
+            barrierLabel: 'Dialg',
             barrierDismissible: true,
             context: context,
             pageBuilder: (ctx, anim1, anim2) => const VerifiedDialog(),
