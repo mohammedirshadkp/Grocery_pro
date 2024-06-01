6dcf7ad3c8e672e5b9decf22f6efc2d1e46ee69f
Mohammed Irshad <mohammedirshadkp500@gmail.com>
2024-06-01 14:14:30 +0530

    review page updated

--- lib/views/review/review_page.dart
+++ lib/views/review/review_page.dart
@@ -12,7 +12,7 @@ class ReviewPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: const AppBackButton(),
-        title: const Text('Reiew'),
+        title: const Text('Reiewww'),
       ),
       body: const Column(
         children: [
