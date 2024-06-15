93bf29325d7c10f953d5ed6badc3e44182539ab9
Mohammed Irshad <mohammedirshadkp500@gmail.com>
2024-06-15 15:08:21 +0530

    profile address pages updated

--- lib/views/profile/address/new_address_page.dart
+++ lib/views/profile/address/new_address_page.dart
@@ -124,7 +124,7 @@ class NewAddressPage extends StatelessWidget {
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                  child: const Text('Save Addddddddress'),
+                  child: const Text('Save Adddddddress'),
                   onPressed: () {},
                 ),
               ),
